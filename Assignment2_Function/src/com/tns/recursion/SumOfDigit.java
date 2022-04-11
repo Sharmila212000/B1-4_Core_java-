bb9a0d71d0094b4b6b7d1fcd1220c7db267766b5
Manu <Manu@LAPTOP-IUQA67VN>
2022-04-11 13:03:27 +0530

    First commit

---
+++ Assignment2_Function/src/com/tns/recursion/SumOfDigit.java
@@ -0,0 +1,26 @@
+package com.tns.recursion;
+
+import java.util.Scanner;
+
+public class SumOfDigit {
+	static int i=0;
+	static int sum(int n) {
+		if(n==0) {
+			return 0;
+		}
+		int d=n%10;
+		i=d;
+		return i+sum(n/10);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number to find the sumofdigit value:");
+		int n=s.nextInt();
+		System.out.println("The sum of digit value of "+n+" is :"+sum(n));
+		s.close();
+
+	}
+
+}
